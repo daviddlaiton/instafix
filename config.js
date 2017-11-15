099e4fe2cee82e36b08488e4e3c35ba80d350fe5
daviddlaiton <ad.laiton10@uniandes.edu.co>
2017-11-15 14:28:43 -0500

    Arreglos generales

--- config.js
+++
@@ -1,6 +0,0 @@
- module.exports = {
- 	'port': process.env.PORT || 8080,
- 	'database': 'mongodb://user:user@ds121495.mlab.com:21495/pmc',
-  //'database': 'mongodb://localhost:27017/pmc',
-  'secret': 'ilovescotchscotchyscotchscotch'
- };
